0bf5699a2e98a5b35db685f28d8a8d01757e2bc8
MD. Sabrim Faraz Prottoy <86478493+SFProttoy@users.noreply.github.com>
2023-07-19 12:34:12 +0600

    work on section 5

---
+++ components/Main/Sections/Section4.jsx
@@ -0,0 +1,228 @@
+const Section4 = () => {
+  return (
+    <section className="section-4">
+      <div className="sec-4-left">
+        <h1 className="common-h1">Nemo enim ipsam quia</h1>
+        <p>
+          Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
+          consectetur, adipisci velit, sed qu nemo enim
+        </p>
+        <div className="option">
+          <svg
+            width="25"
+            height="31"
+            viewBox="0 0 30 31"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g clip-path="url(#clip0_2_4482)">
+              <path
+                d="M15.0005 0.76416C6.71458 0.76416 0 7.47874 0 15.7647C0 24.0476 6.71458 30.7602 15.0005 30.7602C23.2844 30.7602 30 24.0466 30 15.7647C30.001 7.47874 23.2844 0.76416 15.0005 0.76416ZM15.0005 28.2574C8.10023 28.2574 2.50574 22.6659 2.50574 15.7647C2.50574 8.86639 8.10023 3.2679 15.0005 3.2679C21.8998 3.2679 27.4933 8.86539 27.4933 15.7647C27.4943 22.6659 21.8998 28.2574 15.0005 28.2574Z"
+                fill="#B4ECD2"
+              />
+              <path
+                d="M11 16.4579L14.0309 18.9616C14.5267 19.3712 15.2746 19.2131 15.5622 18.6379L19 11.7622"
+                stroke="#B4ECD2"
+                stroke-width="2"
+                stroke-linecap="round"
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_2_4482">
+                <rect
+                  width="30"
+                  height="30"
+                  fill="white"
+                  transform="translate(0 0.762207)"
+                />
+              </clipPath>
+            </defs>
+          </svg>
+          <p>Sed ut perspiciatis unde</p>
+        </div>
+        <div className="option">
+          <svg
+            width="25"
+            height="31"
+            viewBox="0 0 30 31"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g clip-path="url(#clip0_2_4482)">
+              <path
+                d="M15.0005 0.76416C6.71458 0.76416 0 7.47874 0 15.7647C0 24.0476 6.71458 30.7602 15.0005 30.7602C23.2844 30.7602 30 24.0466 30 15.7647C30.001 7.47874 23.2844 0.76416 15.0005 0.76416ZM15.0005 28.2574C8.10023 28.2574 2.50574 22.6659 2.50574 15.7647C2.50574 8.86639 8.10023 3.2679 15.0005 3.2679C21.8998 3.2679 27.4933 8.86539 27.4933 15.7647C27.4943 22.6659 21.8998 28.2574 15.0005 28.2574Z"
+                fill="#B4ECD2"
+              />
+              <path
+                d="M11 16.4579L14.0309 18.9616C14.5267 19.3712 15.2746 19.2131 15.5622 18.6379L19 11.7622"
+                stroke="#B4ECD2"
+                stroke-width="2"
+                stroke-linecap="round"
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_2_4482">
+                <rect
+                  width="30"
+                  height="30"
+                  fill="white"
+                  transform="translate(0 0.762207)"
+                />
+              </clipPath>
+            </defs>
+          </svg>
+          <p>Sed ut perspiciatis unde</p>
+        </div>
+        <div className="option">
+          <svg
+            width="25"
+            height="31"
+            viewBox="0 0 30 31"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g clip-path="url(#clip0_2_4482)">
+              <path
+                d="M15.0005 0.76416C6.71458 0.76416 0 7.47874 0 15.7647C0 24.0476 6.71458 30.7602 15.0005 30.7602C23.2844 30.7602 30 24.0466 30 15.7647C30.001 7.47874 23.2844 0.76416 15.0005 0.76416ZM15.0005 28.2574C8.10023 28.2574 2.50574 22.6659 2.50574 15.7647C2.50574 8.86639 8.10023 3.2679 15.0005 3.2679C21.8998 3.2679 27.4933 8.86539 27.4933 15.7647C27.4943 22.6659 21.8998 28.2574 15.0005 28.2574Z"
+                fill="#B4ECD2"
+              />
+              <path
+                d="M11 16.4579L14.0309 18.9616C14.5267 19.3712 15.2746 19.2131 15.5622 18.6379L19 11.7622"
+                stroke="#B4ECD2"
+                stroke-width="2"
+                stroke-linecap="round"
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_2_4482">
+                <rect
+                  width="30"
+                  height="30"
+                  fill="white"
+                  transform="translate(0 0.762207)"
+                />
+              </clipPath>
+            </defs>
+          </svg>
+          <p>Sed ut perspiciatis unde</p>
+        </div>
+        <div className="option">
+          <svg
+            width="25"
+            height="31"
+            viewBox="0 0 30 31"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g clip-path="url(#clip0_2_4482)">
+              <path
+                d="M15.0005 0.76416C6.71458 0.76416 0 7.47874 0 15.7647C0 24.0476 6.71458 30.7602 15.0005 30.7602C23.2844 30.7602 30 24.0466 30 15.7647C30.001 7.47874 23.2844 0.76416 15.0005 0.76416ZM15.0005 28.2574C8.10023 28.2574 2.50574 22.6659 2.50574 15.7647C2.50574 8.86639 8.10023 3.2679 15.0005 3.2679C21.8998 3.2679 27.4933 8.86539 27.4933 15.7647C27.4943 22.6659 21.8998 28.2574 15.0005 28.2574Z"
+                fill="#B4ECD2"
+              />
+              <path
+                d="M11 16.4579L14.0309 18.9616C14.5267 19.3712 15.2746 19.2131 15.5622 18.6379L19 11.7622"
+                stroke="#B4ECD2"
+                stroke-width="2"
+                stroke-linecap="round"
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_2_4482">
+                <rect
+                  width="30"
+                  height="30"
+                  fill="white"
+                  transform="translate(0 0.762207)"
+                />
+              </clipPath>
+            </defs>
+          </svg>
+          <p>Sed ut perspiciatis unde</p>
+        </div>
+      </div>
+      <div className="sec-4-right">
+        <div className="d-flex">
+          <div className="left">
+            <div className="span"></div>
+            <div className="span"></div>
+            <div className="span"></div>
+            <div className="span"></div>
+            <div className="span"></div>
+            <div className="span-last"></div>
+          </div>
+          <div className="right">
+            <div className="chart">
+              <svg
+                width="635"
+                height="145"
+                viewBox="0 0 639 149"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M2 147L75.1946 108.067C79.2695 105.9 84.0505 105.483 88.4394 106.911L145.292 125.415C150.625 127.151 156.472 126.145 160.917 122.726L220.79 76.6763C226.464 72.3124 234.264 71.9701 240.298 75.8202L314.498 123.161C319.406 126.292 325.574 126.694 330.847 124.228L442.136 72.1575C445.924 70.3852 450.232 70.0714 454.237 71.2759L553.495 101.131C560.528 103.246 568.124 100.606 572.329 94.586L637 2"
+                  stroke="#58BF73"
+                  stroke-width="4"
+                  stroke-linecap="round"
+                />
+              </svg>
+              <svg
+                width="634"
+                height="92"
+                viewBox="0 0 638 96"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M2 94L77.0761 68.623C79.9376 67.6558 83.0052 67.4684 85.9632 68.0803L147.886 80.8882C151.574 81.651 155.41 81.1676 158.794 79.5139L223.066 48.1004C227.405 45.9794 232.442 45.8045 236.918 47.6194L316.353 79.8262C319.928 81.2754 323.889 81.4661 327.586 80.3668L443.346 45.9477C445.955 45.1723 448.711 45.0342 451.383 45.545L556.048 65.5506C561.302 66.5549 566.723 65.0275 570.679 61.4275L636 2"
+                  stroke="#FFC907"
+                  stroke-width="4"
+                  stroke-linecap="round"
+                />
+              </svg>
+            </div>
+
+            <table className="table-2">
+              <thead>
+                <tr>
+                  <th>Source</th>
+                  <th>Campaign</th>
+                  <th>Cost</th>
+                  <th>Sales</th>
+                  <th>Revenue</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Google</td>
+                  <td>Logoipsum data</td>
+                  <td>$150</td>
+                  <td>14</td>
+                  <td className="column-5">$2716</td>
+                </tr>
+
+                <tr>
+                  <td>Google</td>
+                  <td>Logoipsum data</td>
+                  <td>$150</td>
+                  <td>14</td>
+                  <td className="column-5">$2716</td>
+                </tr>
+
+                <tr>
+                  <td>Google</td>
+                  <td>Logoipsum data</td>
+                  <td>$150</td>
+                  <td>14</td>
+                  <td className="column-5">$2716</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Section4;
