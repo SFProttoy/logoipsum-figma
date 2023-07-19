8e3fa856dd226417dbb7a8ae797b03d26204412e
MD. Sabrim Faraz Prottoy <86478493+SFProttoy@users.noreply.github.com>
2023-07-19 17:35:32 +0600

    footer done

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Banner from "@/components/Banner";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Main from "@/components/Main/Main";
 
@@ -8,6 +9,7 @@ const Home = () => {
       <Header />
       <Banner />
       <Main />
+      <Footer />
     </>
   );
 };
